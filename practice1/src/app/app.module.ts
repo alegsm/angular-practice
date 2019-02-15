00306bd954c619391541c07fc53a4c841fe20f37
Alejandro Sánchez <alegsm7@gmail.com>
2019-02-15 10:14:02 -0500

    refactor

--- practice1/src/app/app.module.ts
+++ practice1/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutes} from './app.routing';
+import { HomeComponent } from './screens/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ practice1/src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import {HomeComponent} from './screens/home/home.component';
+
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
